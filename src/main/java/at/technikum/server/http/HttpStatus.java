5fe9df7d6d9088d7ca3fba2ce1b841cd598fb589
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2023-11-14 15:11:03 +0100

    add http status enum

---
+++ src/main/java/at/technikum/server/http/HttpStatus.java
@@ -0,0 +1,22 @@
+package at.technikum.server.http;
+
+// THOUGHT: Add new relevant status (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
+public enum HttpStatus {
+    OK(200, "OK");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
