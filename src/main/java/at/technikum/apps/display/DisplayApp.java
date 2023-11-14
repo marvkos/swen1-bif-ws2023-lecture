4a95554acc599e1e4bb03af908b04b50f54394d0
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2023-11-14 15:44:38 +0100

    add simple display app

--- src/main/java/at/technikum/apps/display/DisplayApp.java
+++ src/main/java/at/technikum/apps/display/DisplayApp.java
@@ -1,14 +1,44 @@
 package at.technikum.apps.display;
 
 import at.technikum.server.ServerApplication;
+import at.technikum.server.http.HttpContentType;
+import at.technikum.server.http.HttpStatus;
 import at.technikum.server.http.Request;
 import at.technikum.server.http.Response;
+
+import java.text.SimpleDateFormat;
 
 public class DisplayApp implements ServerApplication {
 
     @Override
     public Response handle(Request request) {
-        // TODO
-        return null;
+
+        if (request.getRoute().equals("/hello")) {
+            Response response = new Response();
+            response.setStatus(HttpStatus.OK);
+            response.setContentType(HttpContentType.TEXT_PLAIN);
+            response.setBody("World");
+
+            return response;
+        }
+
+        if (request.getRoute().equals("/time")) {
+            Response response = new Response();
+            response.setStatus(HttpStatus.OK);
+            response.setContentType(HttpContentType.TEXT_PLAIN);
+
+            String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss")
+                    .format(new java.util.Date());
+            response.setBody(timeStamp);
+
+            return response;
+        }
+
+        Response response = new Response();
+        response.setStatus(HttpStatus.NOT_FOUND);
+        response.setContentType(HttpContentType.TEXT_PLAIN);
+        response.setBody("Route " + request.getRoute() + " not found in app!");
+
+        return response;
     }
 }
--- src/main/java/at/technikum/server/http/HttpStatus.java
+++ src/main/java/at/technikum/server/http/HttpStatus.java
@@ -2,7 +2,8 @@ package at.technikum.server.http;
 
 // THOUGHT: Add new relevant status (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
 public enum HttpStatus {
-    OK(200, "OK");
+    OK(200, "OK"),
+    NOT_FOUND(404, "Not Found");
 
     private final int code;
     private final String message;
